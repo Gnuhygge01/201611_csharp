cca5e2ea43c236c524acee1fcfb97cccef689330
Michell Cronberg <devcronberg@gmail.com>
2016-11-28 12:16:30 +0100

    06 Ini

---
+++ 06IntroOOP/Person.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace _06IntroOOP
+{
+    public class Person
+    {
+        public string Fornavn;
+        public string Efternavn;
+        private int Alder;
+        private string Adresse;
+
+        // constructor ??
+        public void InitialiserPerson(string fornavn, string efternavn,int alder, string adresse) {
+            this.Fornavn = fornavn;
+            this.Efternavn = efternavn;
+            this.Alder = alder;
+            this.Adresse = adresse;
+
+        }
+
+        public void GemIDatabase()
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
--- 06IntroOOP/Program.cs
+++ 06IntroOOP/Program.cs
@@ -1,8 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace _06IntroOOP
 {
@@ -10,6 +6,47 @@ namespace _06IntroOOP
     {
         static void Main(string[] args)
         {
+
+            Skakbrik s;
+            s = new Skakbrik("Bonde");
+            Skakbrik j = new Skakbrik("Hest");
+
+            
+            
+
+            Skakbrik k;
+            k = s;
+
+            System.Random r = new Random();
+
+            Person p;
+            p = new Person();
+            p.InitialiserPerson("x", "y", 10, "hjh");
+
+            
         }
+    }
+
+    class Skakbrik
+    {
+
+        private DateTime start;
+        private string brikType;
+
+
+        //// ctor + tab
+        //public Skakbrik()
+        //{
+        //    this.start = DateTime.Now;
+        //    // this ikke nødvendigt
+        //    brikType = "ikke angivet";
+        //}
+
+        public Skakbrik(string brikType)
+        {
+            this.start = DateTime.Now;
+            this.brikType = brikType;
+        }
+
     }
 }
