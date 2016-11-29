616554b45c8ea0f1281fd9352aa31a7e161d5911
Michell Cronberg <devcronberg@gmail.com>
2016-11-29 15:05:25 +0100

    .

---
+++ 09Exception/BeregningsException.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _09Exception
+{
+    class BeregningsException : ApplicationException
+    {
+        public BeregningsException()
+        {
+            
+        }
+    }
+}
--- 09Exception/Program.cs
+++ 09Exception/Program.cs
@@ -10,6 +10,54 @@ namespace _09Exception
     {
         static void Main(string[] args)
         {
+
+            throw new ApplicationException("der er sket en fejl");
+
+
+
+
+            try
+            {
+                int y = 10;
+                A();
+
+            }
+
+            catch (Exception ex)
+            {
+                // log ex....
+            }
+            finally
+            {
+                // evt luk/ryd op
+            }
         }
+        public static void A()
+        {
+            B();
+
+        }
+
+        public static int es() {
+            return 1;
+        }
+        public static void B()
+        {
+            C();
+
+        }
+        public static void C()
+        {
+            D();
+        }
+        public static void D()
+        {
+            object o = null;
+            o.ToString();
+        }
+
     }
+
+
 }
+
