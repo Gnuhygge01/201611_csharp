cc9662e9cd88aeee3a7734aeda5bb61365cdea39
Michell Cronberg <devcronberg@gmail.com>
2016-11-29 09:06:58 +0100

    .

---
+++ 07Prop/B.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _07Prop
+{
+    class B
+    {
+        // Offentligt felt (nej tak)
+        public string Navn;
+
+        
+        // start
+        public string Land { get; private set; }
+
+        //private string land;
+
+        //public string Land
+        //{
+        //    get {
+        //        return land; }
+        //    set {
+        //        land = value; }
+        //}
+
+
+
+        public B()
+        {
+
+        }
+
+        public B(string land)
+        {
+            this.Land = land;
+        }
+
+    }
+}
