de9e97bddd92698ffd7a7af9cf584c5daa8507be
DevCronberg <devcronberg@gmail.com>
2016-12-09 12:03:32 +0100

    .

--- 12Events/Program.cs
+++ 12Events/Program.cs
@@ -8,8 +8,85 @@ namespace _12Events
 {
     class Program
     {
+        public delegate void DelegateTest(string tekst);
+        public delegate int BeregnerDelegate(int a, int b);
+
         static void Main(string[] args)
         {
+
+            DelegateTest funcRef = new DelegateTest(Test);
+            Test("1");
+            funcRef.Invoke("2");
+
+            DelegateTest funcRef2 = Test;
+            funcRef2 += Test2;
+            funcRef2.Invoke("2");
+
+            BeregnerDelegate beregner = Add;
+            beregner += Sub;
+            Console.ForegroundColor = ConsoleColor.Yellow;
+            foreach (var f in beregner.GetInvocationList())
+            {
+                Console.WriteLine(f.DynamicInvoke(5, 5));
+            }
+
+            Console.WriteLine();
+            var fu = FindFunktion();
+            Console.WriteLine(fu(5, 5));
+            Console.WriteLine(FindFunktion()(4, 4));
+
+
+            //
+
+            Action<string> minA = Test;
+            minA += Test2;
+            minA("xfljldjdkjfg");
+
+            Action<string, int, DateTime> minB;
+
+            // sidste args definerer returtypen
+            Func<int, int, int> MinBeregner = Add;
+            MinBeregner(5, 4);
+
+            Predicate<int> testp = (i) => i < 10;
+            var e = testp(1);
+
+
         }
+
+        static BeregnerDelegate FindFunktion() {
+            if (DateTime.Now.Millisecond % 2 == 0) {
+                return Add;
+            } else {
+                return Sub;
+            }
+        }
+
+        static void MinLog(string t, DelegateTest f) {
+            // logik...
+            f(t);
+            // logik...
+        }
+
+
+        static int Add(int i, int x) {
+            return i + x;
+        }
+
+        static int Sub(int i, int x)
+        {
+            return i - x;
+        }
+
+        static void Test(string txt) {
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.WriteLine(txt);
+        }
+
+        static void Test2(string txt) {
+            Console.ForegroundColor = ConsoleColor.Blue;
+            Console.WriteLine(txt);
+        }
+
     }
 }
